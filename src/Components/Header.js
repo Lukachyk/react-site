a8620d72e0c526b941227fd2c6751f8c191670a7
Roman Lukachyk <petrovroman26@icloud.com>
2023-01-05 12:54:11 +0200

    0.0.2 add BrouserRouter

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,15 +7,19 @@ import {
   Form,
   Button,
 } from "react-bootstrap";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../Pages/Home";
+import Contacts from "../Pages/Contacts";
+import Blog from "../Pages/Blog";
+import About from "../Pages/About";
 import logo from "./logo192.png";
 
 export default class Header extends Component {
   render() {
     return (
-      <>
+      <BrowserRouter>
         <Navbar
-          fixed="top"
+          sticky="top"
           collapseOnSelect
           expand="md"
           bg="primary"
@@ -51,7 +55,13 @@ export default class Header extends Component {
             </Navbar.Collapse>
           </Container>
         </Navbar>
-      </>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/Pages/Blog.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Blog extends Component {
+  render() {
+    return <div>Hello Blog</div>;
+  }
+}
+
+export default Blog;
