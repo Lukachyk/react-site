04205e5c642c68abc35201a9ebb814625d8426d2
Roman Lukachyk <petrovroman26@icloud.com>
2023-01-05 17:25:34 +0200

    0.0.6 add contact us

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -1,8 +1,32 @@
 import React, { Component } from "react";
+import { Form, Button, Container } from "react-bootstrap";
 
 export class Contacts extends Component {
   render() {
-    return <div>Hello Contacts</div>;
+    return (
+      <Container style={{ width: "500px" }}>
+        <h1 className="text-center">Contact us</h1>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text>
+              We'll never share your email with any more else
+            </Form.Text>
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Example textarea</Form.Label>
+            <Form.Control as="textarea" rows="3" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    );
   }
 }
 
